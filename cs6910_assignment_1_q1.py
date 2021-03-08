ed7ddae1cc4a4caf5d3a556c279728b53bb0e4b1
Parimal Kumar <cooldudesnehal1@gmail.com>
2021-03-08 23:02:21 +0530

    corrected solution to q1

--- cs6910_assignment_1_q1.py
+++ cs6910_assignment_1_q1.py
@@ -14,13 +14,13 @@ from keras.datasets import fashion_mnist
 #displaying 1 image from each category
 classes = {0 : 'T-shirt/top', 1 : 'Trouser', 2 : 'Pullover', 3 : 'Dress', 4 : 'Coat', 5 : 'Sandal', 6 : 'Shirt', 7 : 'Sneaker', 8 : 'Bag', 9 : 'Ankle Boot'}
 counter = 0
-for i in range(y_test.shape[0]):
-  if y_test[i] in classes and counter < 10:
-    ax = plt.subplot(4, 4, y_test[i]+1)
+for i in range(y_train.shape[0]):
+  if y_train[i] in classes and counter < 10:
+    ax = plt.subplot(4, 4, y_train[i]+1)
     img = x_train[i]
-    ax.set_title(classes[y_test[i]])
+    ax.set_title(classes[y_train[i]])
     ax.axis('off')
     plt.tight_layout()
     plt.imshow(img, cmap='gray')
-    del(classes[y_test[i]])
+    del(classes[y_train[i]])
     counter += 1
