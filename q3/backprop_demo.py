corrected few gradient calculations and updates

q3/backprop_demo.py
@@ -7,12 +7,12 @@ import numpy as np
 import myDLkit #importing our self-defined header containing the feed forward neural network class
 
 #definiing hyper parameters of the nn to be created
-n_inputs = 10
-n_hidden_layers = 8
+n_inputs = 784
+n_hidden_layers = 20
 n_outputs = 10
 
 #defining a test input 
-in_row_vector = np.zeros([1, 10])
+in_row_vector = np.zeros([1, n_inputs])
 for i in range(n_inputs):
 	in_row_vector[0, i] = i
 
@@ -30,14 +30,15 @@ for i in nn.grad_wrt_b:
 
 #updating the weights post calculation of grads by backprop
 for i in nn.grad_wrt_W:
-	nn.layers[i].W = nn.layers[i].W - nn.grad_wrt_W[i].T
+	nn.layers[i].W = nn.layers[i].W - nn.grad_wrt_W[i]
 
+'''
+#This section can be uncommented to view the results.	
 #printing the updated weights and biases
 for i in nn.grad_wrt_b:
 	print(nn.layers[i].b)
 
 for i in nn.grad_wrt_W:
-	print(nn.layers[i].W)
+	print(nn.layers[i].W) 
+'''
 
-
-		

q3/myDLkit.py
@@ -121,7 +121,7 @@ class feed_fwd_nn:
 
 	def backProp(self, input_vector, true_label):
 		y_hat = self.forwardProp(input_vector)
-		y_actual = np.zeros([1, 10])
+		y_actual = np.zeros([1, self.n_outputs])
 		y_actual[0, true_label] = 1
 		#gradient with respect to output layer for cross entropy loss
 		grad_vec_wrt_output = -(y_actual.T - y_hat.T);#(kx1)
@@ -137,7 +137,7 @@ class feed_fwd_nn:
 		for i in range(self.n_inputs):
 			a_grad_last[i, 0] = h_grad_last[i,0] * self.last_hidden_layer_h[0, i] * (1 - self.last_hidden_layer_h[0, i])
 		self.grad_wrt_a[self.n_hidden_layers] = a_grad_last	  	
-		self.grad_wrt_b[self.n_hidden_layers] = self.grad_wrt_a[self.n_hidden_layers]
+		self.grad_wrt_b[self.n_hidden_layers] = grad_vec_wrt_output
 		self.grad_wrt_W[self.n_hidden_layers] = np.dot(self.grad_wrt_a[self.n_hidden_layers+1],self.last_hidden_layer_h)
 		
 		for i in range(self.n_hidden_layers-1, 0, -1):
@@ -149,4 +149,4 @@ class feed_fwd_nn:
 			self.grad_wrt_b[i] = self.grad_wrt_a[i] 
 			self.grad_wrt_W[i] = np.dot(self.grad_wrt_a[i+1], self.grad_wrt_h[i].T) 
 		self.grad_wrt_W[0] = np.dot(self.grad_wrt_a[1], (np.dot(self.layers[0].W.T, self.grad_wrt_a[1])).T)
-	
+		